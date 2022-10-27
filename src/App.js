1acd640ece5331385a0b47562bf976fb16eefee4
Karin Nordkvist <kontakt@karinnordkvist.se>
2022-10-27 23:28:30 +0200

    ph1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <img class="logo" src="./images/fjellmans-logo.svg" />
+        <h1 class="title">Frisör</h1>
+        <div class="tel-box">
+          <h2 class="ring">Ring:</h2>
+          <h2 class="number">023-222 20</h2>
+        </div>
+
+        <div class="adress-box">
+          <h3 class="adress">ORNÄSGATAN 21 C</h3>
+          <h3 class="">791 62 FALUN</h3>
+        </div>
+
+        <img class="main-image" src="./images/fjellmans.jpg" />
+        <img class="icon" src="./images/fjellmans-ikon.svg" />
+      </main>
     </div>
   );
 }
